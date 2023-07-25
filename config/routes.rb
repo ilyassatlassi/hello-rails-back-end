24fea053e9425bed6faf2c213deeafe5fe69c0a5
ilyass atlassi <atlassiilyass@gmail.com>
2023-07-25 18:16:21 +0100

    Create greeting api

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      get '/random_greeting', to: 'greetings#random'
+    end
+  end
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+greetings = [
+    'Hello',
+    'Hi',
+    'Greetings',
+    'Salutations',
+    'Hey there'
+  ]
+  
+  greetings.each do |greeting|
+    Message.create(content: greeting)
+  end
